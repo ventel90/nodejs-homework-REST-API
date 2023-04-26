12937ee13b282ca806700e7bba9a139b655a44d5
VItaliy Guivan <107477405+ventel90@users.noreply.github.com>
2023-04-26 22:42:28 +0300

    Jimp added.

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const gravatar = require('gravatar');
 const path = require('path');
 const fs = require('fs/promises');
+const Jimp = require('jimp');
 
 const { User } = require('../models/user');
 
@@ -92,6 +93,8 @@ const updateAvatar = async (req, res) => {
   const { path: tempUpload, originalname } = req.file;
   const filename = `${_id}_${originalname}`;
   const resultUpload = path.join(avatarsDir, filename);
+  const originalAvatar = await Jimp.read(tempUpload);
+  originalAvatar.resize(250, 250).write(tempUpload);
   await fs.rename(tempUpload, resultUpload);
   const avatarUrl = path.join("avatars", filename);
   await User.findByIdAndUpdate(_id, { avatarUrl });
